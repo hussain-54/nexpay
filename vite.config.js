7a230f432579b04afe9be2cb23b2f3df61ff6174
hussain-54 <hussaindurrani92@gmail.com>
2026-05-10 03:23:51 +0500

    fix: remove broken rollup-plugin-node-polyfills that crashes Vite 6

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [react()],
   define: {
     "process.env": {},
     global: "globalThis",
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: { global: "globalThis" },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({ process: true, buffer: true }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()],
-    },
   },
 });
